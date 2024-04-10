1f6806f7e60061eec5164424e70f7bcd67cc4f41
Evan <evan.lueber@stud.kbw.ch>
2024-04-10 14:43:37 +0200

    update readme

--- README.md
+++ README.md
@@ -3,9 +3,18 @@ My own UI Library with components I would like to use in other projects.
 
 ## Components
 ### Password Input
+#### Use The Component
 A password input component with a show/hide password button.
+Make sure you have TailwindCSS installed in your project if you want to use custom styles.
 ```jsx
+// Import the PasswordInput component
 import { PasswordInput } from 'elu-ui';
 
+// Use the PasswordInput component
 <PasswordInput />
 ```
+#### Props
+```jsx	
+<PasswordInput value={} onChange={} placeholder="" />
+```
+If you want to add custom styles you can use the props 'custom' or just for the font 'customFont'. In there you can only use tailwind
--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -23,8 +23,8 @@ export const PasswordInput: React.FC<PasswordInputProps> = ({
   return (
     <div
       className={
-        " flex flex-row gap-0 h-20 w-3/4 justify-center items-center bg-black/50 rounded-lg shadow-md shadow-black md:h-1/2 focus-within:border-b-[#0BCAAD] focus-within:border-b-2 focus-within:shadow-none md:pt-2 md:pb-2 " +
-        actualSize
+        actualSize +
+        " flex flex-row gap-0 h-20 w-3/4 justify-center items-center bg-black/50 rounded-lg shadow-md shadow-black md:h-1/2 focus-within:border-b-[#0BCAAD] focus-within:border-b-2 focus-within:shadow-none md:pt-2 md:pb-2 " 
       }
     >
       <input
@@ -32,8 +32,8 @@ export const PasswordInput: React.FC<PasswordInputProps> = ({
         value={value}
         onChange={onChange}
         className={
-          " text-white text-xl md:text-3xl outline-none border-none p-0 bg-transparent focus:outline-transparent focus:border-transparent flex-grow w-full ml-4 " +
-          fontSize
+          fontSize +
+          " text-white text-xl md:text-3xl outline-none border-none p-0 bg-transparent focus:outline-transparent focus:border-transparent flex-grow w-full ml-4 " 
         }
         placeholder={placeholder}
       />
